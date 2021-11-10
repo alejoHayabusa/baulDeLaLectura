bbde9c44410ca2f9c89c29d29f862328aca0d24a
alejoHayabusa <36050570+alejoHayabusa@users.noreply.github.com>
2021-11-10 16:40:08 -0500

    Add files via upload

---
+++ js/libros.js
@@ -0,0 +1,71 @@
+function cientifico () {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("cientifico").style.display = 'inline';
+    document.getElementById("linguistico").style.display = 'none';
+    document.getElementById("artistico").style.display = 'none';
+    document.getElementById("lenguaje").style.display = 'none';
+    document.getElementById("CSociales").style.display = 'none';
+    document.getElementById("zonaLibre").style.display = 'none';
+
+    document.getElementById("TLibros").innerHTML = '<strong>Cientificos</strong>';
+}
+
+function linguistico () {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("cientifico").style.display = 'none';
+    document.getElementById("linguistico").style.display = 'inline';
+    document.getElementById("artistico").style.display = 'none';
+    document.getElementById("lenguaje").style.display = 'none';
+    document.getElementById("CSociales").style.display = 'none';
+    document.getElementById("zonaLibre").style.display = 'none';
+
+    document.getElementById("TLibros").innerHTML = '<strong>Lingüísticos</strong>';
+}
+
+function artistico () {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("cientifico").style.display = 'none';
+    document.getElementById("linguistico").style.display = 'none';
+    document.getElementById("artistico").style.display = 'inline';
+    document.getElementById("lenguaje").style.display = 'none';
+    document.getElementById("CSociales").style.display = 'none';
+    document.getElementById("zonaLibre").style.display = 'none';
+
+    document.getElementById("TLibros").innerHTML = '<strong>Artísticos</strong>';
+}
+
+function lenguaje () {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("cientifico").style.display = 'none';
+    document.getElementById("linguistico").style.display = 'none';
+    document.getElementById("artistico").style.display = 'none';
+    document.getElementById("lenguaje").style.display = 'inline';
+    document.getElementById("CSociales").style.display = 'none';
+    document.getElementById("zonaLibre").style.display = 'none';
+
+    document.getElementById("TLibros").innerHTML = '<strong>Lengüaje</strong>';
+}
+
+function CSociales () {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("cientifico").style.display = 'none';
+    document.getElementById("linguistico").style.display = 'none';
+    document.getElementById("artistico").style.display = 'none';
+    document.getElementById("lenguaje").style.display = 'none';
+    document.getElementById("CSociales").style.display = 'inline';
+    document.getElementById("zonaLibre").style.display = 'none';
+
+    document.getElementById("TLibros").innerHTML = '<strong>Ciencias Sociales</strong>';
+}
+
+function zonaLibre () {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("cientifico").style.display = 'none';
+    document.getElementById("linguistico").style.display = 'none';
+    document.getElementById("artistico").style.display = 'none';
+    document.getElementById("lenguaje").style.display = 'none';
+    document.getElementById("CSociales").style.display = 'none';
+    document.getElementById("zonaLibre").style.display = 'inline';
+
+    document.getElementById("TLibros").innerHTML = '<strong>Zona Libre</strong>';
+}
